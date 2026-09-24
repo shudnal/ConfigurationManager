1aa20231530ca36ebacf5f09e8cb482418b3ff7b
Suden <20457922+shudnal@users.noreply.github.com>
2026-09-24 14:04:24 +0300

    Bump version to 1.1.22

--- ConfigurationManager.cs
+++ ConfigurationManager.cs
@@ -21,7 +21,7 @@ namespace ConfigurationManager
     {
         public const string GUID = "_shudnal.ConfigurationManager";
         public const string pluginName = "Valheim Configuration Manager";
-        public const string Version = "1.1.21";
+        public const string Version = "1.1.22";
 
         internal static ConfigurationManager instance;
         private static SettingFieldDrawer _fieldDrawer;
